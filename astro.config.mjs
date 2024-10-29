b42f28c0243a7095386b6089aedaddd6aaae0893
dlsubzh <joshua.binswanger@gmail.com>
2024-10-29 13:52:32 +0100

    added keen slider

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,3 +7,12 @@ export default defineConfig({
   site: 'https://rhizome.ch',
   integrations: [robotsTxt()],
 });
+
+import { imageService } from "@unpic/astro/service";
+export default defineConfig({
+  image: {
+    service: imageService({
+      placeholder: "lqip",
+   }),
+  },
+});
