d1c3b715ae7a9441ab8ae352ed66e3919db4e951
dlsubzh <joshua.binswanger@gmail.com>
2024-11-01 15:38:31 +0100

    first commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,13 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection } from 'astro:content';
 import { z } from 'zod';
+import { glob } from 'astro/loaders';
+import { cldAssetsLoader } from 'astro-cloudinary/loaders';
 
 
 // 2. Define your collection(s)
-const galleryCollection = defineCollection({ 
-    type: 'data',
+/* const gallery = defineCollection({ 
+  loader: glob({ pattern: '**\/[^_]*.yaml', base: "./src/content/gallery" }),
   schema: z.object({
     projectName: z.string(),
     title: z.string(),
@@ -17,23 +19,47 @@ const galleryCollection = defineCollection({
 
     images: z.array(
       z.object({
-        src: z.string(),
-        alt: z.string(),
-        width: z.string().optional(),
         loading: z.enum(['eager', 'lazy']).optional(), // Define 'loading' as a Zod enum
         to: z.string().optional(),
-        description: z.string(),
-        imageTags: z.array(z.string()).optional(),
       })
     ), // Array of image objects
   }),
-});
+}); */
+export const collections = { 
+  galleryImages: defineCollection({
+    loader: cldAssetsLoader({
+      context: true,
+      folder: 'Portfolio/Gallery/Xylopedia',
+      tags: true,
+      metadata: true,
+    })
+  }),
 
+  gallery: defineCollection({ 
+    loader: glob({ pattern: '**\/[^_]*.yaml', base: "./src/content/gallery" }),
+    schema: z.object({
+      projectName: z.string(),
+      title: z.string(),
+      subtitle: z.string(),
+      tags: z.array(z.string()),
+      bgcolor: z.string().optional(),
+      projectDescription: z.string(),
+      credits: z.string(),
+  
+      images: z.array(
+        z.object({
+          loading: z.enum(['eager', 'lazy']).optional(), // Define 'loading' as a Zod enum
+          to: z.string().optional(),
+        })
+      ), // Array of image objects
+    }),
+  }),
+
+}
 
 
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
-export const collections = {
-  'gallery': galleryCollection,
-};
+
+
