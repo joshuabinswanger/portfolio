e87525460ae3f0bb1b89e70141cb67d12f595b2f
dlsubzh <joshua.binswanger@gmail.com>
2024-11-01 23:12:30 +0100

    added env PUBLIC in CLoudflare

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { glob } from 'astro/loaders';
 import { cldAssetsLoader } from 'astro-cloudinary/loaders';
 
+const cloudName = import.meta.env.PUBLIC_CLOUDINARY_CLOUD_NAME;
+
+
 
 export const collections = { 
   galleryImages: defineCollection({
