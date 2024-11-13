39cc599d0ebde8fbbc875d18c020c312e0887849
dlsubzh <joshua.binswanger@gmail.com>
2024-11-13 01:15:39 +0100

    changed InfoSlide

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -235,6 +235,11 @@ declare module 'astro:content' {
   rendered?: RenderedContent;
   filePath?: string;
 }>;
+"projects": Record<string, {
+  id: string;
+  collection: "projects";
+  data: any;
+}>;
 
 	};
 
