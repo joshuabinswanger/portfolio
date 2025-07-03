5253c309c8e817ad82c9070c9bfdd3fee1dd0b6a
joshuabinswanger <113670425+joshuabinswanger@users.noreply.github.com>
2025-07-03 18:05:36 +0200

    eslint

--- .pnp.cjs
+++ .pnp.cjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-/* eslint-disable */
+ 
 // @ts-nocheck
 "use strict";
 
@@ -14426,7 +14426,7 @@ function arg(
     }
 
     if (wholeArg.length > 1 && wholeArg[0] === "-") {
-      /* eslint-disable operator-linebreak */
+       
       const separatedArguments =
         wholeArg[1] === "-" || wholeArg.length === 2
           ? [wholeArg]
@@ -14434,7 +14434,7 @@ function arg(
               .slice(1)
               .split("")
               .map((a) => `-${a}`);
-      /* eslint-enable operator-linebreak */
+       
 
       for (let j = 0; j < separatedArguments.length; j++) {
         const arg = separatedArguments[j];
@@ -14477,7 +14477,7 @@ function arg(
               !(
                 argv[i + 1].match(/^-?\d*(\.(?=\d))?\d*$/) &&
                 (type === Number ||
-                  // eslint-disable-next-line no-undef
+                   
                   (typeof BigInt !== "undefined" && type === BigInt))
               ))
           ) {
