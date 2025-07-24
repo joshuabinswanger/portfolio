02a317b374d46753fef4987c80e78099cc83ef51
dlsubzh <joshua.binswanger@gmail.com>
2025-07-24 14:38:41 +0200

    added sortProjectsByYear function

---
+++ src/components/list/utils.ts
@@ -0,0 +1,14 @@
+import { getEntry, getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+
+
+/**
+ * Sort All Projects by date (ascending)
+ * @param projects - Array of projects collection entries to sort
+ * @returns New sorted array of projects
+ */
+export function sortProjectsByYear<T extends CollectionEntry<"projects">>(
+    projects: T[]
+  ): T[] {
+    return [...projects].sort((a, b) => b.data.year.localeCompare(a.data.year));
+  }
