91885872f2503770b8ec78cd44491b9c520ca8b7
dlsubzh <joshua.binswanger@gmail.com>
2025-07-24 20:48:37 +0200

    cleaned galler utils.ts changed og Image

--- src/components/list/utils.ts
+++ src/components/list/utils.ts
@@ -1,13 +1,9 @@
 import { getEntry, getCollection } from "astro:content";
 import type { CollectionEntry } from "astro:content";
 
-/**
- * Sort All Projects by date (ascending)
- * @param projects - Array of projects collection entries to sort
- * @returns New sorted array of projects
- */
-export function sortProjectsByYear<T extends CollectionEntry<"projects">>(
-  projects: T[],
-): T[] {
+
+export function sortProjectsByYear(
+  projects: CollectionEntry<"projects">,
+) {
   return [...projects].sort((a, b) => b.data.year.localeCompare(a.data.year));
 }
